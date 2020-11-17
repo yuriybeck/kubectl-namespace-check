78cc5ffc0bee7731b7693eb804fff03dcf14e6db
Yuriy S <dev@solovyov.de>
2020-11-17 11:39:36 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,10 +7,13 @@ then
   helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
   helm repo add stable https://charts.helm.sh/stable
   helm repo update
-  kubectl create --namespace prometheus
+  kubectl create namespace prometheus
+  
+  #generate password
   htpasswd -b -c prometheus-basic-auth prometheus ${PROM_AUTH_PASSWORD}
   kubectl create secret -n prometheus generic basic-auth --from-file=prometheus-basic-auth
-  kubectl create -f monitoring/prometheus/cloud_issuer.yaml
+  
+  # deploy prometheus
   helm upgrade --install prometheus prometheus-community/prometheus -n prometheus --create-namespace -f monitoring/prometheus/values.yaml \
         --set-string server.ingress.hosts=prometheus-${CLOUD_ENVIRONMENT}.${MONITORING_DOMAIN} \
         --set-string server.ingress.tls[0].hosts=prometheus-${CLOUD_ENVIRONMENT}.${MONITORING_DOMAIN} \
